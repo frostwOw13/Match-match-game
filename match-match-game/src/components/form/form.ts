40f991aeaa5063ffc34a9e769ba07040436d969d
frostwOw13 <frostwow13@yandex.ru>
2021-05-25 20:48:09 +0300

    feat: add styles to form

--- match-match-game/src/components/form/form.ts
+++ match-match-game/src/components/form/form.ts
@@ -6,26 +6,49 @@ export class Form extends BaseComponent {
     super('div', ['form']);
 
     this.element.innerHTML = `
-      <h3 class='form__title'>Register new player</h3>
+      <div class="form__container">
+        <div class="form__header">
+          <h3>Register new player</h3>
+        </div>
 
-      <div class='form__container'>
-        <form class='input-container'>
-          <label for="firstName">first name</label>
-          <input type="text" name="firstName" value="insert your first name">
+        <div class="form-wrapper">
+          <form id="form" class="form">
 
-          <label for="secondName">secondName</label>
-          <input type="text" name="secondName" value="insert your seconds name">
+            <div class="form-control">
+              <label for="firstName">First name</label>
+              <input type="text" placeholder="Jones" id="firstName" />
+              <i class="fas fa-check-circle"></i>
+              <i class="fas fa-exclamation-circle"></i>
+              <small>Error message</small>
+            </div>
 
-          <label for="email">Email</label>
-          <input type="text" name="email" value="insert your email">
-        </form>
+            <div class="form-control">
+              <label for="secondName">Second name</label>
+              <input type="text" placeholder="Dermot" id="secondName" />
+              <i class="fas fa-check-circle"></i>
+              <i class="fas fa-exclamation-circle"></i>
+              <small>Error message</small>
+            </div>
 
-        <div class='image-container'></div>
-      </div>
+            <div class="form-control">
+              <label for="email">Email</label>
+              <input type="email" placeholder="example@gmail.com" id="email"/>
+              <i class="fas fa-check-circle"></i>
+              <i class="fas fa-exclamation-circle"></i>
+              <small>Error message</small>
+            </div>
 
-      <div class="form__buttons">
-        <button id="add" class="btn">add user</button>
-        <button id="cancel" class="btn">cancel</button>
+            <button id="cancel" class="btn">cancel</button>
+            <button id="add" class="btn">add user</button>
+            
+          </form>
+
+          <div class="form__picture">
+            <div alt="load your picture" class="picture__img"></div>
+          </div>
+
+        </div>
+
       </div>
     `;
   }
